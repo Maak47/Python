902a9a496282ec975a912c96783aef9070ed425c
Oshaf Aalam Khan (Maak) <oshaf.khan10@gmail.com>
2024-08-28 16:49:09 +0530

    Update coinFlipStreak.py
    added new method

--- ABSWP/Problems/coinFlipStreak.py
+++ ABSWP/Problems/coinFlipStreak.py
@@ -1,4 +1,26 @@
-# CoinflipStreak(string based)
+#CoinFlipStreak(list based)
+
+import random
+
+numberOfStreaks = 0
+
+for experimentNumber in range(10000):
+    CoinFlip = [random.randint(0,1) for i in range(100)]
+    
+    streak = 1
+    for i in range(1, len(CoinFlip)):
+        if CoinFlip[i] == CoinFlip[i - 1]:
+            streak += 1
+        else: 
+            streak = 1
+        if streak == 6:
+            numberOfStreaks += 1
+            break 
+
+print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+
+
+# CoinflipStreak(string based 1 )
 import random
 numberOfStreaks = 0
 for experimentNumber in range(10000):
