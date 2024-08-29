33dc856beff1df2302d4adaaa5157f044ae50c49
Maak47 <oshaf.khan10@gmail.com>
2024-08-29 21:02:27 +0530

    fantasyGameInventory part 2 complete

--- ABSWP/Problems/fantasyGameInventory.py
+++ ABSWP/Problems/fantasyGameInventory.py
@@ -1,15 +1,23 @@
 #fantasy game inventory
 
-inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12,
-             }
+# inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12,
+#              }
+
 
 def displayInventory(inventory):
     print('Inventory:')
     item_total = 0
     for k, v in inventory.items():
-        #work on this
         print(str(v) + ' ' + k)
         item_total += v
     print('Total number of items: ' + str(item_total))
-    
-displayInventory(inventory)
+
+def addToInventory(inventory, addedItems):
+    for item in addedItems:
+        inventory[item] = inventory.get(item, 0) + 1 
+
+inv = {'gold coin' : 42, 'rope' : 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+print(addToInventory(inv, dragonLoot))
+displayInventory(inv)
