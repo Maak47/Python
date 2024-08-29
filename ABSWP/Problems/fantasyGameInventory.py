be887adc8797247f8e404bb067a262931cae3870
Maak47 <oshaf.khan10@gmail.com>
2024-08-29 20:33:06 +0530

    fanstasyGameInventory part 1 complete

---
+++ ABSWP/Problems/fantasyGameInventory.py
@@ -0,0 +1,15 @@
+#fantasy game inventory
+
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12,
+             }
+
+def displayInventory(inventory):
+    print('Inventory:')
+    item_total = 0
+    for k, v in inventory.items():
+        #work on this
+        print(str(v) + ' ' + k)
+        item_total += v
+    print('Total number of items: ' + str(item_total))
+    
+displayInventory(inventory)
