daa38fbf9816b134f0409e0ecc72ea6fc0d3280d
Maak47 <oshaf.khan10@gmail.com>
2024-08-29 20:25:30 +0530

    project question complete

--- ABSWP/Problems/chessDictionaryValidator.py
+++ ABSWP/Problems/chessDictionaryValidator.py
@@ -1,35 +1,40 @@
-chessBoard = {'1h': 'bking', '6c': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e':'wking', '8d': 'bking',
+chessBoard = {'1h': 'bking', '6c': 'wqueen', '7d': 'wqueen', '2g': 'bbishop', '5h': 'bqueen', '3e':'wking', '8d': 'bking',
 }
 
 def isValidChessBoard(board):
+    #one white king and one black king
+    if 'wking' not in board.values() or 'bking' not in board.values():
+        print('You have more than one black or white king')
+        return False
+    
+    #total number of pieces
+    if len(board.values()) > 32:
+        print('You have more than 32 pieces on board')
+        return False
 
+    #most 8 pawns both players
+    piece_count = {}
+    for piece in board.values():
+        piece_count[piece] = piece_count.get(piece, 0) + 1
+
+    #pieces of pawn
+    if piece_count.get('wpawn', 0) > 8 or piece_count.get('bpawn', 0) > 8:
+        print('You have more than 8 pieces of either black or white pawn')
+        return False
+
+    #position
     for position in board.keys():
         if int(position[0]) > 8 or position[1] not in 'abcdefgh':
-            print('Board is INVALID, the piece can\'t be on a position thats 9 and greater or not in a-h')
+            print('Pieces not on board, position is wrong')
             return False
-    
-    if 'bking' not in board.values() or 'wking' not in board.values():
-        print('Board is INVALID because the kings are not present')
-        return False
-    
-    if len(board.values()) > 16:
-        print('Board is INVALID, there are more pieces than 16')
-        return False
-    
-    if list(board.values()).count('wpawn') > 8 or list(board.values()).count('bpawn') > 8:
-        print('Board is INVALID, because of more pieces of pawn than 8')
-        return False
 
-    for piece in ['wbishop', 'bbishop', 'wknight', 'bknight', 'wrook', 'brook']:
-        if list(board.values()).count(piece) > 2:
-            print('Board is INVALID, because of more pieces of bishop, knight or rook than 2 each player')
+    #pieces count        
+    for piece, maxCount in [('wbishop', 2), ('bbishop', 2), ('wknight', 2), ('bknight', 2), ('wrook', 2), ('brook', 2), ('wqueen', 1), ('bqueen', 1)]:
+        if piece_count.get(piece, 0) > maxCount:
+            print(f'More pieces of {piece} than {maxCount}')
             return False
-        
-    for piece in ['wqueen', 'bqueen']:
-        if list(board.values()).count(piece) > 1:
-            print('Board is INVALID, because of more pieces of wqueen or bqueen than 1')
-            return False
-    
+
     return True
 
 print(isValidChessBoard(chessBoard))
+
