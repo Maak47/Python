f97f148afd8ff7f585aa963602567e0c3cfb0b22
Maak47 <oshaf.khan10@gmail.com>
2024-08-30 17:15:47 +0530

    chap 6 start

---
+++ ABSWP/catnapping.py
@@ -0,0 +1,18 @@
+print(
+'''
+Dear Alice,
+
+Eve's cat has been arrested for catnapping, cat burglary, and extortion.
+
+Sincerely,
+
+Bob
+'''
+)
+
+"""
+This is a test Python Program.
+Written by Al Sweigart al@inventwithPython.com
+
+Thiss program was designed for Python 3, not Python 2.
+"""
